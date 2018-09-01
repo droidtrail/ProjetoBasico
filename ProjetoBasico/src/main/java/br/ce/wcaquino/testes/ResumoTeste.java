b6ab15e00587163a5fce1467cc96de6dab7099db
droidtrail <31170024+droidtrail@users.noreply.github.com>
2018-09-01 05:21:37 -0300

    Desafio: Teste do resumo Mensal Vazio. Seção 9, aula 90.

--- ProjetoBasico/src/main/java/br/ce/wcaquino/testes/ResumoTeste.java
+++ ProjetoBasico/src/main/java/br/ce/wcaquino/testes/ResumoTeste.java
@@ -2,15 +2,16 @@ package br.ce.wcaquino.testes;
 
 import static br.ce.wcaquino.core.DriverFactory.getDriver;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
-
-import com.sun.xml.internal.bind.v2.runtime.Name;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import br.ce.wcaquino.core.BaseTest;
-import br.ce.wcaquino.core.DriverFactory;
 import br.ce.wcaquino.pages.MenuPage;
 import br.ce.wcaquino.pages.ResumoPage;
 
@@ -38,7 +39,9 @@ public class ResumoTeste extends BaseTest {
 		menuPage.acessarTelaResumo();
 
 		Assert.assertEquals("Seu Barriga - Extrato", getDriver().getTitle());
-
+	
+		List<WebElement> elementosEncontrados = getDriver().findElements(By.xpath(".//table[@id='tabelaExtrato']/tbody/tr"));
+		Assert.assertEquals(0, elementosEncontrados.size());	
 	}
 
 }
