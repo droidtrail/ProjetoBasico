d6d653533f41cd15de770a7a203bb547fcd95b00
droidtrail <31170024+droidtrail@users.noreply.github.com>
2018-09-01 02:05:58 -0300

    Teste: Resumo Mensal. Seção 9 - Aula 86.

--- ProjetoBasico/src/main/java/br/ce/wcaquino/testes/ResumoTeste.java
+++ ProjetoBasico/src/main/java/br/ce/wcaquino/testes/ResumoTeste.java
@@ -1,27 +1,39 @@
 package br.ce.wcaquino.testes;
 
+import static br.ce.wcaquino.core.DriverFactory.getDriver;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import br.ce.wcaquino.core.BaseTest;
+import br.ce.wcaquino.core.DriverFactory;
 import br.ce.wcaquino.pages.MenuPage;
 import br.ce.wcaquino.pages.ResumoPage;
 
 public class ResumoTeste extends BaseTest {
-	
+
 	MenuPage menuPage = new MenuPage();
 	ResumoPage resumoPage = new ResumoPage();
-	
+
 	@Test
 	public void TesteExcluirMovimentacao() {
-		
+
 		menuPage.acessarTelaResumo();
-		
+
 		resumoPage.excluirMovimentacao();
-	
+
 		Assert.assertEquals("Movimentação removida com sucesso!",
 				resumoPage.obterMensagemMovimentacaoExcluidaSucesso());
-		
+
+	}
+
+	@Test
+	public void testeResumoMensal() {
+
+		menuPage.acessarTelaResumo();
+
+		Assert.assertEquals("Seu Barriga - Extrato", getDriver().getTitle());
+
 	}
 
 }
