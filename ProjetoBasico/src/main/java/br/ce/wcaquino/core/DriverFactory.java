309dcb66758009867e4565a19f2930244209516e
droidtrail <31170024+droidtrail@users.noreply.github.com>
2018-09-06 22:11:05 -0300

    Refatorando os testes para usar o GRID. Seção 10 - Aula 100. Estabilizada.

--- ProjetoBasico/src/main/java/br/ce/wcaquino/core/DriverFactory.java
+++ ProjetoBasico/src/main/java/br/ce/wcaquino/core/DriverFactory.java
@@ -5,82 +5,40 @@ import java.net.URL;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import br.ce.wcaquino.core.Propriedades.TipoExecucao;
 
 public class DriverFactory {
-
-	// private static WebDriver driver;
-	private static ThreadLocal<WebDriver> theadDriver = new ThreadLocal<WebDriver>() {
-
+	
+	private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<WebDriver>(){
 		@Override
-		protected synchronized WebDriver initialValue() {
-
+		protected synchronized WebDriver initialValue(){
 			return initDriver();
-
 		}
-
 	};
-
-	private DriverFactory() {
+	
+	private DriverFactory() {}
+	
+	public static WebDriver getDriver(){
+		return threadDriver.get();
 	}
-
-	public static WebDriver getDriver() {
-
-		return theadDriver.get();
-	}
-
-	public static WebDriver initDriver() {
-
+	
+	public static WebDriver initDriver(){
 		WebDriver driver = null;
-
-		if (Propriedades.TIPO_EXECUCAO == TipoExecucao.LOCAL) {
-
+		if(Propriedades.TIPO_EXECUCAO == TipoExecucao.LOCAL) {
 			switch (Propriedades.BROWSER) {
-			case FIREFOX:
-				System.setProperty("webdriver.gecko.driver", "e:\\GeckoDriver\\geckodriver.exe");
-				driver = new FirefoxDriver();
-				break;
-			case CHROME:
-				System.setProperty("webdriver.chrome.driver", "E:\\ChromeDriver_win32\\chromedriver.exe");
-				driver = new ChromeDriver();
-				break;
-
-			case INTERNETEXPLORER:
-				System.setProperty("webdriver.ie.driver", "E:\\ieDriver\\IEDriverServer.exe");
-				driver = new InternetExplorerDriver();
-				break;
-
-			case EDGE:
-				System.setProperty("webdriver.edge.driver", "E:\\EdgeDriver\\MicrosoftWebDriver.exe");
-				driver = new EdgeDriver();
-				break;
-
+				case FIREFOX: driver = new FirefoxDriver(); break;
+				case CHROME: driver = new ChromeDriver(); break;
 			}
 		}
-		if (Propriedades.TIPO_EXECUCAO == TipoExecucao.GRID) {
-			
+		if(Propriedades.TIPO_EXECUCAO == TipoExecucao.GRID) {
 			DesiredCapabilities cap = null;
-			
 			switch (Propriedades.BROWSER) {
-				case FIREFOX: 
-					
-					cap = DesiredCapabilities.firefox();
-					System.setProperty("webdriver.gecko.driver", "e:\\GeckoDriver\\geckodriver.exe");
-					driver = new FirefoxDriver();
-				
-				break;
-				case CHROME: 
-					cap = DesiredCapabilities.chrome();
-					System.setProperty("webdriver.chrome.driver", "E:\\ChromeDriver_win32\\chromedriver.exe");
-					driver = new ChromeDriver();
-					
-					break;	
+				case FIREFOX: cap = DesiredCapabilities.firefox(); break;
+				case CHROME: cap = DesiredCapabilities.chrome(); break;
 			}
 			try {
 				driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
@@ -89,21 +47,18 @@ public class DriverFactory {
 				e.printStackTrace();
 			}
 		}
-
-		driver.manage().window().maximize();
+		driver.manage().window().maximize();		
 		return driver;
 	}
 
-	public static void killDriver() {
-		// Se o WebDriver existir, encerra o processo
+	public static void killDriver(){
 		WebDriver driver = getDriver();
-		if (driver != null) {
+		if(driver != null) {
 			driver.quit();
 			driver = null;
 		}
-		// Se a threadDriver existir, encerra a thread
-		if (theadDriver != null) {
-			theadDriver.remove();
+		if(threadDriver != null) {
+			threadDriver.remove();
 		}
 	}
 }
--- ProjetoBasico/src/main/java/br/ce/wcaquino/core/Propriedades.java
+++ ProjetoBasico/src/main/java/br/ce/wcaquino/core/Propriedades.java
@@ -4,7 +4,7 @@ public class Propriedades {
 	
 	public static boolean FECHAR_BROWSER = true;
 	
-	public static Browsers BROWSER = Browsers.FIREFOX;
+	public static Browsers BROWSER = Browsers.CHROME;
 	
 	public static TipoExecucao TIPO_EXECUCAO = TipoExecucao.GRID;
 	
